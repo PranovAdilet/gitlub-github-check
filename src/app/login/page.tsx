8cccae72686ab598fe91469a67dfb8d02733fd1f
Nursultan <nurlanov0105@gmail.com>
2024-04-11 21:57:08 +0600

    text

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Page = () => {
-   return <div>page</div>;
+   return (
+      <div>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores laborum non dolore
+         cupiditate, repellendus repudiandae laudantium. Cumque quisquam ullam mollitia possimus
+         eius voluptatum vero consequuntur deleniti veniam, eos vitae distinctio!
+      </div>
+   );
 };
 
 export default Page;
