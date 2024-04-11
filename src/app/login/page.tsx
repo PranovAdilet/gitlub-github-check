193097c5e31f020b1a14281f3f8f3bcee03524ab
Nursultan <nurlanov0105@gmail.com>
2024-04-11 21:59:55 +0600

    testsd

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+   return (
+      <div>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores laborum non dolore
+         cupiditate, repellendus repudiandae laudantium. Cumque quisquam ullam mollitia possimus
+         eius voluptatum vero consequuntur deleniti veniam, eos vitae distinctio! sdsdsdsdcsdd
+      </div>
+   );
+};
+
+export default Page;
